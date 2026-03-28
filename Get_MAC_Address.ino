dfff0c9e1ac9512c863ab8cab1faef8753e4fdc6
Mahinur26 <mahinurmahi26@gmail.com>
2026-03-28 12:12:06 -0400

    Document

--- Get_MAC_Address.ino
+++ Get_MAC_Address.ino
@@ -1,13 +1,10 @@
 #include <WiFi.h>
-
+// Will return a MAC address that will be used to connect the ESP32's to each other with ESP-NOW
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
   WiFi.mode(WIFI_MODE_STA);
-  // REMOVE this line:
-  // WiFi.disconnect(true);
-
   delay(1000);
 
   Serial.println("ESP32 MAC Address:");
